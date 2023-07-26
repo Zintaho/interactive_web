cabb0a13ac7b0fcfc91d18cf5105031330d88b25
Zintaho <zintaho@gmail.com>
2023-07-26 12:52:37 +0900

    Part 2 / Chapter 2 / Clip 4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import CanvasOption from "./js/CanvasOption.js";
 import Particle from "./js/Particle.js";
-import { randomNumBetween } from "./js/Utils.js";
+import { hypotenuse, randomNumBetween } from "./js/Utils.js";
 
 class Canvas extends CanvasOption {
     constructor() {
@@ -22,13 +22,17 @@ class Canvas extends CanvasOption {
     }
 
     createParticles() {
-        const PARTICLE_NUM = 1000;
+        const PARTICLE_NUM = 400;
         const x = randomNumBetween(0, this.canvasWidth);
         const y = randomNumBetween(0, this.canvasHeight);
         for (let i = 0; i <PARTICLE_NUM; ++i) {
-            const vx = randomNumBetween(-5, 5);
-            const vy = randomNumBetween(-5, 5);
-            this.particles.push(new Particle(x,y,vx,vy));
+            const r = randomNumBetween(2, 100) * hypotenuse(innerWidth, innerHeight) * 0.0001;
+            const angle = Math.PI / 180 * randomNumBetween(0, 360);
+            const opacity = randomNumBetween(0.6, 0.9);
+
+            const vx = r * Math.cos(angle);
+            const vy = r * Math.sin(angle);
+            this.particles.push(new Particle(x,y,vx,vy,opacity));
         }
     }
 
@@ -42,7 +46,7 @@ class Canvas extends CanvasOption {
             now = Date.now();
             delta = now - TouchEvent;
             if (delta < this.interval) return;
-            this.ctx.fillStyle = this.bgColor;
+            this.ctx.fillStyle = this.bgColor + '40';
             this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
     
             this.particles.forEach((particle, index) => {
