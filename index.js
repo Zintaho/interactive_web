b6af861c79fe7fcf6ca4a90446fbc7320087fcfd
Zintaho <zintaho@gmail.com>
2023-07-28 12:58:53 +0900

    Part 2 / Chapter 2 / Clip 5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import CanvasOption from "./js/CanvasOption.js";
 import Particle from "./js/Particle.js";
+import Tail from "./js/Tail.js";
 import { hypotenuse, randomNumBetween } from "./js/Utils.js";
 
 class Canvas extends CanvasOption {
@@ -7,6 +8,7 @@ class Canvas extends CanvasOption {
         super();
 
         this.particles = [];
+        this.tails = [];
     }
 
     init() {
@@ -21,10 +23,8 @@ class Canvas extends CanvasOption {
         this.createParticles();
     }
 
-    createParticles() {
+    createParticles(x, y, color) {
         const PARTICLE_NUM = 400;
-        const x = randomNumBetween(0, this.canvasWidth);
-        const y = randomNumBetween(0, this.canvasHeight);
         for (let i = 0; i <PARTICLE_NUM; ++i) {
             const r = randomNumBetween(2, 100) * hypotenuse(innerWidth, innerHeight) * 0.0001;
             const angle = Math.PI / 180 * randomNumBetween(0, 360);
@@ -32,8 +32,15 @@ class Canvas extends CanvasOption {
 
             const vx = r * Math.cos(angle);
             const vy = r * Math.sin(angle);
-            this.particles.push(new Particle(x,y,vx,vy,opacity));
+            this.particles.push(new Particle(x,y,vx,vy,opacity,color));
         }
+    }
+
+    createTail() {
+        const x= randomNumBetween(this.canvasWidth * 0.2, this.canvasWidth * 0.8);
+        const vy = this.canvasHeight * randomNumBetween(0.012, 0.015) * -1;
+        const color = `${randomNumBetween(0,255)}, ${randomNumBetween(0,255)}, ${randomNumBetween(0,255)}`;
+        this.tails.push(new Tail(x, vy, color));
     }
 
     render() {
@@ -49,6 +56,17 @@ class Canvas extends CanvasOption {
             this.ctx.fillStyle = this.bgColor + '40';
             this.ctx.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
     
+            if (Math.random() < 0.03) this.createTail();
+            this.tails.forEach((tail, index) => {
+                tail.update();
+                tail.draw();
+
+                if (tail.vy > -0.5) {
+                    this.tails.splice(index, 1);
+                    this.createParticles(tail.x, tail.y, tail.color);
+                }
+            });
+
             this.particles.forEach((particle, index) => {
                 particle.update();
                 particle.draw();
